54cb13b1b8dc3e7c711efb0321d5ae5f3fdee00a
misraag <jaiofficial50@gmail.com>
2025-08-31 13:31:54 +0530

    started working on frontend, navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import BackendTest from './components/BackendTest'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-      <BackendTest/>
+      {/* <BackendTest/> */}
+      <Navbar/>
     </>
   )
 }
