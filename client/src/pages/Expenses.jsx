6caaab98ab12571ab3e1e08658a740a8baa3b576
misraag <jaiofficial50@gmail.com>
2025-08-31 17:42:53 +0530

    refactored some repeating logic

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -1,33 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Expenses() {
-    let [expenses, setExpenses] = useState([])
+  let [expenses, setExpenses] = useState([]);
 
-    useEffect(()=>{
-        const token = localStorage.getItem("token")
-        axios.get("http://localhost:5000/api/expenses", {headers: {Authorization: `Bearer ${token}`}})
-        .then((res)=> {
-            console.log(res)
-            setExpenses(res.data)
-        })
-        .catch((err)=> console.log(err.response.data))
-    },[])
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios
+      .get("http://localhost:5000/api/expenses", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        console.log(res);
+        setExpenses(res.data);
+      })
+      .catch((err) => console.log(err.response.data));
+  }, []);
 
-
-    return (
-        <div className="container mt-4">
+  return (
+    <div className="container mt-4">
       <h2>Your Expenses</h2>
       <ul className="list-group mt-3">
-        {expenses.map(exp => (
-          <li key={exp._id} className="list-group-item d-flex justify-content-between align-items-center">
-            {exp.category}: {exp.description}
-            <span className="badge bg-success rounded-pill">₹{exp.amount}</span>
-          </li>
-        ))}
+        {expenses.length > 0 ? (
+          expenses.map((exp) => (
+            <li
+              key={exp._id}
+              className="list-group-item d-flex justify-content-between align-items-center"
+            >
+              {exp.category}: {exp.description}
+              <span className="badge bg-success rounded-pill">
+                ₹{exp.amount}
+              </span>
+            </li>
+          ))
+        ) : (
+          <p>No expense added</p>
+        )}
       </ul>
     </div>
-    );
+  );
 }
 
 export default Expenses;
